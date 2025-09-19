ffe03157d0eb3ff632a17eb978a79dcc13f61b66
Blinkx-Original <martinpino@outlook.com>
2025-09-19 17:04:27 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,56 +1,15 @@
 import * as React from 'react';
-
-import { homePageBookSumState, homePageQueryState } from 'atoms';
-
-import CommonLayout from 'components/v2/Layout';
-import { FilteredChips } from 'components/v2/Chips/FilteredChips';
 import Head from 'next/head';
-import type { NextPage } from 'next';
-import { PAGE_SIZE } from 'const';
-import Pagination from 'components/v2/Pagination';
-import { Suspense } from 'react';
+import CommonLayout from 'components/v2/Layout';
 import dynamic from 'next/dynamic';
-import { useRecoilState } from 'recoil';
 
-const BookList = dynamic(import('components/v2/Cards/ShoppingItemCardList'), { ssr: false })
+const ProductList = dynamic(() => import('components/v2/Cards/ShoppingItemCardList'), { ssr: false });
 
-const Home: NextPage = () => {
-  const [homePageQueryData, setHomePageQueryData] =
-    useRecoilState(homePageQueryState);
-  const [homePageBookSum] = useRecoilState(homePageBookSumState);
-
-  const handleClickPagination = (page: number) => {
-    setHomePageQueryData({ ...homePageQueryData, page });
-  };
+export default function HomePage() {
+  // paginación simple local
+  const [page, setPage] = React.useState(1);
+  const pageSize = 12;
 
   return (
-    <>
-      <Head>
-        <title>Bookstore Home</title>
-        <meta name='description' content='Bookstore Home Page' />
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
+    <
 
-      <CommonLayout>
-        {(homePageQueryData.sort || homePageQueryData.type) && (
-          <FilteredChips
-            data={homePageQueryData}
-            onChange={setHomePageQueryData}
-          />
-        )}
-        <Suspense fallback={<div>Loading...</div>}>
-          <BookList page={homePageQueryData?.page || 1} pageSize={PAGE_SIZE} />
-        </Suspense>
-        <div className='flex justify-center pt-6'>
-          <Pagination
-            currentPage={homePageQueryData?.page || 1}
-            pages={Math.round(homePageBookSum / PAGE_SIZE)}
-            onClick={handleClickPagination}
-          />
-        </div>
-      </CommonLayout>
-    </>
-  );
-};
-
-export default Home;
