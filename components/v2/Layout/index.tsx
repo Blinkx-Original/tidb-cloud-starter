14c61cb0bbed929cfbd01f7c0eaadc693de75ce4
Blinkx-Original <martinpino@outlook.com>
2025-09-19 18:32:32 +0200

    Update index.tsx

--- components/v2/Layout/index.tsx
+++ components/v2/Layout/index.tsx
@@ -1,19 +1,18 @@
 import * as React from 'react';
 import Header, { HeaderProps } from './Header';
+import Footer from '../Footer';
 
 export interface LayoutProps {
   children: React.ReactNode;
-  headerProps?: HeaderProps; // <-- ahora el layout acepta props para el header
+  headerProps?: HeaderProps;
 }
 
 export default function CommonLayout({ children, headerProps }: LayoutProps) {
   return (
     <div className="min-h-screen flex flex-col">
-      {/* Global Header en todas las páginas */}
       <Header {...(headerProps || {})} />
-
-      {/* Contenido de la página */}
       <main className="flex-1">{children}</main>
+      <Footer />
     </div>
   );
 }
