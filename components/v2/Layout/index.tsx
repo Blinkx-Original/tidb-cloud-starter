9d53d7fe2604f4bc4a1d72f3e9df309929de6bdf
Blinkx-Original <martinpino@outlook.com>
2025-09-19 16:57:08 +0200

    Update index.tsx

--- components/v2/Layout/index.tsx
+++ components/v2/Layout/index.tsx
@@ -1,36 +1,18 @@
 import * as React from 'react';
-import NextLink from 'next/link';
-import {
-  Bars3Icon,
-  MagnifyingGlassIcon,
-  ShoppingBagIcon,
-  XMarkIcon,
-  ShoppingCartIcon,
-  UserIcon,
-} from '@heroicons/react/24/outline';
+import Header from './Header';
 
-import Header, { HeaderProps } from 'components/v2/Layout/Header';
-
-export interface CommonLayoutProps {
-  children?: any;
-  headerProps?: HeaderProps;
+export interface LayoutProps {
+  children: React.ReactNode;
 }
 
-export default function CommonLayout(props: CommonLayoutProps) {
-  const { headerProps, children } = props;
-
+export default function CommonLayout({ children }: LayoutProps) {
   return (
-    <>
-      <div className='min-h-full'>
-        <Header {...headerProps} />
+    <div className="min-h-screen flex flex-col">
+      {/* Global Header */}
+      <Header />
 
-        <main>
-          <div className='mx-auto max-w-7xl py-6 px-4'>
-            {/* Your content */}
-            {children}
-          </div>
-        </main>
-      </div>
-    </>
+      {/* Page content */}
+      <main className="flex-1">{children}</main>
+    </div>
   );
 }
