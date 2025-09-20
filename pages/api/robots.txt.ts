fb492037c5a490894b4f616c289467197e256723
Blinkx-Original <martinpino@outlook.com>
2025-09-20 14:56:15 +0200

    Create robots.txt.ts

---
+++ pages/api/robots.txt.ts
@@ -0,0 +1,12 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(_req: NextApiRequest, res: NextApiResponse) {
+  res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+  const host = process.env.NEXT_PUBLIC_SITE_URL || 'https://example.com';
+  res.send(
+`User-agent: *
+Allow: /
+Sitemap: ${host}/sitemap.xml
+`
+  );
+}
