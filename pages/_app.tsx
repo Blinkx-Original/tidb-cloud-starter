f9d8aa41e25ee6c2cd74acdd14483ca2c65934dc
Blinkx-Original <martinpino@outlook.com>
2025-09-21 06:46:08 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,14 @@
-// pages/_app.tsx
 import type { AppProps } from 'next/app';
 import { RecoilRoot } from 'recoil';
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/next';
 import 'styles/globals.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
       <Component {...pageProps} />
-      {/* Vercel Analytics: funciona en cualquier dominio */}
+      {/* Vercel Analytics works on any domain/env */}
       <Analytics />
-      <SpeedInsights />
     </RecoilRoot>
   );
 }
