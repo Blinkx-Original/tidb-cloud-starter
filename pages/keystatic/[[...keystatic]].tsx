e24c2de9c81bd5eade287ec0971401e889cd5bbe
Blinkx-Original <martinpino@outlook.com>
2025-09-21 09:17:25 +0200

    Delete pages/keystatic/[[...keystatic]].tsx

--- pages/keystatic/[[...keystatic]].tsx
+++
@@ -1,6 +0,0 @@
-// pages/keystatic/[[...keystatic]].tsx  ← CREAR
-// Admin UI de Keystatic (Pages Router)
-import { makePage } from '@keystatic/next/ui/pages';
-import config from '../../keystatic.config';
-
-export default makePage(config);
