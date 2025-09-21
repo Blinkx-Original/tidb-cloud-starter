9bc63f2f3f2727f7cf0cf2b2ffbf9c9d319011a0
Blinkx-Original <martinpino@outlook.com>
2025-09-21 11:50:28 +0200

    Update [[...keystatic]].tsx

--- pages/keystatic/[[...keystatic]].tsx
+++ pages/keystatic/[[...keystatic]].tsx
@@ -1,5 +1,5 @@
 // pages/keystatic/[[...keystatic]].tsx
-// Admin UI for Keystatic (Pages Router)
 import { makePage } from '@keystatic/next/ui/pages';
 import config from '../../keystatic.config';
+
 export default makePage(config);
