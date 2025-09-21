27e222858d10494e15fa40571d011687fd2d62e1
Blinkx-Original <martinpino@outlook.com>
2025-09-21 15:49:55 +0200

    Delete pages/api/keystatic/[[...params]].ts

--- pages/api/keystatic/[[...params]].ts
+++
@@ -1,9 +0,0 @@
-// pages/api/keystatic/[[...params]].ts
-import { makeAPIRouteHandler } from '@keystatic/next/api';
-import type { NextApiRequest, NextApiResponse } from 'next';
-import config from '../../../keystatic.config';
-
-export default makeAPIRouteHandler({ config }) as unknown as (
-  req: NextApiRequest,
-  res: NextApiResponse
-) => void;
