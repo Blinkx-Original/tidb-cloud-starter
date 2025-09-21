9ca76679e8d54d1ffcce188b5e0afe51e320d919
Blinkx-Original <martinpino@outlook.com>
2025-09-21 11:54:46 +0200

    Update [[...keystatic]].tsx

--- pages/keystatic/[[...keystatic]].tsx
+++ pages/keystatic/[[...keystatic]].tsx
@@ -1,5 +1,12 @@
-// pages/keystatic/[[...keystatic]].tsx
+// pages/keystatic/[[...keystatic]].tsx  ← REEMPLAZAR (archivo completo)
+// Admin UI de Keystatic (Pages Router) con SSR forzado para evitar prerender en build.
+import type { GetServerSideProps } from 'next';
 import { makePage } from '@keystatic/next/ui/pages';
 import config from '../../keystatic.config';
+
+// Forzamos SSR para que Next no intente SSG de esta ruta (evita "Invalid repo config" en build).
+export const getServerSideProps: GetServerSideProps = async () => {
+  return { props: {} };
+};
 
 export default makePage(config);
