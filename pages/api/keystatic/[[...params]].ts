c2170b857498f0f50c8b18e35ad81764ec8a32ab
Blinkx-Original <martinpino@outlook.com>
2025-09-21 11:49:40 +0200

    Update [[...params]].ts

--- pages/api/keystatic/[[...params]].ts
+++ pages/api/keystatic/[[...params]].ts
@@ -1,8 +1,7 @@
 // pages/api/keystatic/[[...params]].ts
-// API used by the Keystatic Admin UI
 import { makeAPIRouteHandler } from '@keystatic/next/api';
 import type { NextApiRequest, NextApiResponse } from 'next';
-import config from '../../keystatic.config';  // two levels up to reach keystatic.config.ts
+import config from '../../../keystatic.config';
 
 export default makeAPIRouteHandler({ config }) as unknown as (
   req: NextApiRequest,
