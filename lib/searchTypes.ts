8ccce1acf0645f15b01446ca86c52dfa340cbd28
Blinkx-Original <martinpino@outlook.com>
2025-09-22 19:34:24 +0200

    Create searchTypes.ts

---
+++ lib/searchTypes.ts
@@ -0,0 +1,14 @@
+export type HitModel = {
+  objectID: string;
+  title: string;
+  url: string;
+  image?: string;
+  description?: string;
+  category?: string;
+  tags?: string[];
+  price?: number;
+  rating?: number;
+  city?: string;
+  state?: string;
+  country?: string;
+};
