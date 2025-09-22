d26b03a624f50b75c3ae5356169ad711218e67ec
Blinkx-Original <martinpino@outlook.com>
2025-09-22 20:58:09 +0200

    Update index.tsx

--- components/v2/index.tsx
+++ components/v2/index.tsx
@@ -1,3 +1,19 @@
 // components/v2/index.tsx
-import CommonLayout from './Layout';
-export default CommonLayout;
+import * as React from "react";
+import Header from "./Layout/Header";
+import Footer from "../v2/Footer";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function CommonLayout({ children }: Props) {
+  return (
+    <div className="bg-black text-white min-h-screen">
+      <Header />
+      {/* Contenido */}
+      <div>{children}</div>
+      <Footer />
+    </div>
+  );
+}
