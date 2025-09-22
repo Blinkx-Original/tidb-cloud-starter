829c50c167a4b7f7ef76146698d7d361c0ba2e31
Blinkx-Original <martinpino@outlook.com>
2025-09-22 18:33:53 +0200

    Update index.tsx

--- components/v2/Layout/index.tsx
+++ components/v2/Layout/index.tsx
@@ -1,20 +1,20 @@
 // components/v2/Layout/index.tsx
 import * as React from 'react';
-import Header, { HeaderProps } from './Header';
+import Header from './Header';
 import Footer from '../Footer';
 
 export interface LayoutProps {
   children: React.ReactNode;
-  headerProps?: HeaderProps;
 }
 
-export default function CommonLayout({ children, headerProps }: LayoutProps) {
+export default function CommonLayout({ children }: LayoutProps) {
   return (
-    <div className="min-h-screen flex flex-col bg-white text-black">
-      <Header {...(headerProps || {})} />
+    <div className="min-h-screen flex flex-col bg-base-100">
+      <Header />
       <main className="flex-1">{children}</main>
       <Footer />
     </div>
   );
 }
+
 
