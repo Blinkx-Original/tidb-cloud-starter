8e18fab0b9b85f9dee901dd8585438308b98cd5f
Blinkx-Original <martinpino@outlook.com>
2025-09-23 10:30:35 +0200

    Update algoliaSearchClient.ts

--- lib/algoliaSearchClient.ts
+++ lib/algoliaSearchClient.ts
@@ -2,7 +2,11 @@ import algoliasearch, { SearchClient } from 'algoliasearch';
 
 const appId = process.env.NEXT_PUBLIC_ALGOLIA_APP_ID!;
 const searchKey = process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_KEY!;
-const prefix = process.env.NEXT_PUBLIC_ALGOLIA_INDEX_PREFIX || 'catalog';
 
-export const getIndexName = (name = 'items') => `${prefix}__${name}`;
+// NUEVO: si pones el nombre exacto del índice en esta env var, lo usamos tal cual.
+const explicitIndex = process.env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME;
+
+const prefix = process.env.NEXT_PUBLIC_ALGOLIA_INDEX_PREFIX || 'catalog';
+export const getIndexName = (name = 'items') => explicitIndex || `${prefix}__${name}`;
+
 export const searchClient: SearchClient = algoliasearch(appId, searchKey);
