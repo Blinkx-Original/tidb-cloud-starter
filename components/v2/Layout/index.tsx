86f4677084e9c85bb8add71c365ff5937c9507f2
Blinkx-Original <martinpino@outlook.com>
2025-09-23 08:13:45 +0200

    Update index.tsx

--- components/v2/Layout/index.tsx
+++ components/v2/Layout/index.tsx
@@ -1,7 +1,7 @@
 // components/v2/Layout/index.tsx
 import * as React from 'react';
 import Header from './Header';
-import type { HeaderProps } from './Header';
+type HeaderProps = React.ComponentProps<typeof Header>;
 import Footer from '../Footer';
 
 export interface LayoutProps {
@@ -12,11 +12,12 @@ export interface LayoutProps {
 export default function CommonLayout({ children, headerProps }: LayoutProps) {
   return (
     <div className="min-h-screen flex flex-col bg-base-100">
-      <Header {...headerProps} />
+      <Header {...(headerProps ?? {})} />
       <main className="flex-1">{children}</main>
       <Footer />
     </div>
   );
 }
+
 
 
