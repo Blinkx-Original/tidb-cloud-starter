dff5748bc3591e8fa0c3a2d40783eab6b011a635
Blinkx-Original <martinpino@outlook.com>
2025-09-24 17:20:28 +0200

    Update index.tsx

--- components/v2/index.tsx
+++ components/v2/index.tsx
@@ -4,18 +4,16 @@
 import * as React from 'react';
 import Header from './Layout/Header';
 
-type Props = {
-  children: React.ReactNode;
-};
+type Props = { children: React.ReactNode };
 
 export default function CommonLayout({ children }: Props) {
   return (
     <>
       <Header />
       <main className="min-h-[50vh]">{children}</main>
-      <footer className="border-t border-black/10 bg-white">
+      <footer className="border-top border-black/10 bg-white">
         <div className="mx-auto max-w-6xl px-4 py-6 text-sm opacity-70">
-          © {new Date().getFullYear()} BlinkX
+          © {new Date().getFullYear()} Blinkx
         </div>
       </footer>
     </>
