3917601f9fd438c7128d28314f9e440e85055260
Blinkx-Original <martinpino@outlook.com>
2025-09-24 18:41:52 +0200

    Update index.tsx

--- components/v2/index.tsx
+++ components/v2/index.tsx
@@ -8,14 +8,21 @@ type Props = { children: React.ReactNode };
 
 export default function CommonLayout({ children }: Props) {
   return (
-    <>
+    <div className="min-h-screen flex flex-col bg-white text-black isolate">
       <Header />
-      <main className="min-h-[50vh]">{children}</main>
-      <footer className="border-t border-black/10 bg-white" role="contentinfo">
+
+      {/* El contenido crece y empuja el footer hacia abajo */}
+      <main className="flex-1">{children}</main>
+
+      {/* Footer SIEMPRE visible */}
+      <footer
+        className="relative z-10 border-t border-black/10 bg-white"
+        role="contentinfo"
+      >
         <div className="mx-auto max-w-6xl px-4 py-6 text-sm opacity-70">
           © {new Date().getFullYear()} BlinkX
         </div>
       </footer>
-    </>
+    </div>
   );
 }
