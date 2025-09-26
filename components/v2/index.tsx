f7dff41f56994fd27c5f7175d4aa813d67045e25
Blinkx-Original <martinpino@outlook.com>
2025-09-26 15:13:04 +0200

    Update index.tsx

--- components/v2/index.tsx
+++ components/v2/index.tsx
@@ -1,20 +1,23 @@
 // components/v2/index.tsx
-'use client';
+import React, { useEffect } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
 
-import * as React from 'react';
-import Header from './Layout/Header';
-import Footer from './Footer';
+export default function CommonLayout({ children }: { children: React.ReactNode }) {
+  // Fuerza light: quita cualquier "dark" que alguna página/componente haya puesto
+  useEffect(() => {
+    if (typeof document !== "undefined") {
+      document.documentElement.classList.remove("dark");
+      document.body.classList.remove("dark");
+      document.documentElement.setAttribute("data-theme", "light");
+    }
+  }, []);
 
-type Props = { children: React.ReactNode };
-
-export default function CommonLayout({ children }: Props) {
   return (
-    <div className="min-h-screen flex flex-col bg-white text-black isolate">
+    <div data-theme="light" className="min-h-screen bg-white text-black flex flex-col">
       <Header />
       <main className="flex-1">{children}</main>
-      {/* Footer original con enlaces legales, social, etc. */}
       <Footer />
     </div>
   );
 }
-
